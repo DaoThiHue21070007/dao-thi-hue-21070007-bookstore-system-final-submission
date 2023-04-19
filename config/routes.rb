11fbee0ab9e856b438478d25f0d09615f9fd2904
unknown <Admin@DESKTOP-OAEMAEK>
2023-04-19 21:31:30 +0700

    Polishing plate boiler content

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
 
 
   devise_for :users
-  root to: 'products#index'
+  root to: 'home#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
