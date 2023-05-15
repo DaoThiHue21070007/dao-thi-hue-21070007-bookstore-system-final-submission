b7d1eee5330170805158331be03720d537dbc553
unknown <Admin@DESKTOP-OAEMAEK>
2023-05-15 16:35:16 +0700

    Add model validation and association

--- app/models/staff.rb
+++ app/models/staff.rb
@@ -1,3 +1,6 @@
 class Staff < ApplicationRecord
   belongs_to :gender
+  validates_presence_of :code, :name, :date_of_birth, :gender
+ validates_uniqueness_of :code
+ validates_numericality_of :phone
 end
